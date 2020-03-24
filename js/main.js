4a20ce14cfeebbecb8f03976d347e72515949113
Warlen Pinheiro <warlen@live.com>
2020-03-24 08:28:33 -0300

    Remoção de js

--- js/main.js
+++ js/main.js
@@ -19,28 +19,8 @@ $(document).ready(function () {
 
     $('.project-area .button-group #btn1').trigger('click');
 
-    $('.project-area .grid .test-popup-link').magnificPopup({
-        type: 'image',
-        gallery: { enabled: true }
-    });
 
-
-    // Owl-carousel
-
-    $('.site-main .about-area .owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: true,
-        dots: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            560: {
-                items: 2
-            }
-        }
-    })
-
+   
     // sticky navigation menu
 
     let nav_offset_top = $('.header_area').height() + 50;
